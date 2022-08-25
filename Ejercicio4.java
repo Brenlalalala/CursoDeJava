368de11dde635f4d17915218cbd990fe183131b2
Brenda Melgarejo <brendal_la22@hotmail.com>
2022-08-24 23:04:44 -0300

    Ejercicio número 4

---
+++ Ejercicio4.java
@@ -0,0 +1,13 @@
+package EjercicioN4;
+
+public class Ejercicio4 {
+    public static void main(String[] args) {
+        String nombre = "Brenda";
+        int edad=31;
+        double salario= 133000.26;
+        boolean carnet=false;
+
+        System.out.print("Nombre:" + nombre + " " + "Edad:" + edad + " " +" Salario:" + salario + " " + "Carnet:" + carnet);
+
+}
+}
