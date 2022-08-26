3d1793f23b0773a2ee9b6921d79d32b1ffb852bf
Brenda Melgarejo <brendal_la22@hotmail.com>
2022-08-26 18:01:52 -0300

    Creando carpeta con Ejercicios

---
+++ PracticaLab2/Ejercicio6.java
@@ -0,0 +1,28 @@
+package Ejercicio6;
+
+public class Ejercicio6 {
+
+        public static void main(String[] args){
+
+            double x = Math.sin(0.5);
+            double x1 = Math.cos(0.5);
+            double x2 = Math.tan(0.5);
+            double x3 = Math.atan(1);
+            double x4 = Math.exp(2);
+            double x5 = Math.log(2.7172);
+
+            System.out.println("Seno: " +x);
+            System.out.println("Coseno: " +x1);
+            System.out.println("Tangente: " +x2);
+            System.out.println("Arcotangente: " +x3);
+            System.out.println("Exponente: " +x4);
+            System.out.println("Logaritmo: " +x5);
+
+
+            //Las dos constantes PI y e
+            System.out.println("PI es igual a: " + Math.PI);
+            System.out.println("e es igual a: " + Math.E);
+
+        }
+    }
+
